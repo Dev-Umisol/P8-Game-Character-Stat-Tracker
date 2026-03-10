a0c7ab2023d98f530d1c5e99da91ca79d88c413d
Dev-Umisol <148490822+Dev-Umisol@users.noreply.github.com>
2026-03-10 13:11:15 -0400

    Finished Project 8 game character stats tracker

--- P8GameCharacterStatTracker.py
+++ P8GameCharacterStatTracker.py
@@ -1,5 +1,4 @@
 class GameCharacter:
-    
     def __init__(self, name):
         self._name = name
         self._health = 100
@@ -8,7 +7,7 @@ class GameCharacter:
     
     @property
     def name(self):
-        return self._name
+        return self._name # Read-only access getter
         
     @property
     def health(self):
@@ -16,17 +15,45 @@ class GameCharacter:
     
     @health.setter
     def health(self, new_health):
-        if min(new_health) <= 0 or max(new_health) >= 100:
-            raise ValueError("Health can not be set to 0 or greater than 100")
-            
+        if new_health <= 0:
+            new_health = 0 # <-- Sets Health to 0 if user tries and go lower than 0
+        elif new_health >= 100:
+            new_health = 100 #<-- Sets Health to 100 if user tries and go higher than 10-
         
+        self._health = new_health # <-- Sets new_health to whatever number user enters
+            
     @property
     def mana(self):
         return self._mana # Getter
     
     @mana.setter
     def mana(self, new_mana):
-           if self.mana < 0:
-               raise ValueError("Mana can not be set to 0")
-           
+        if new_mana <= 0:
+            new_mana = 0 # <-- Sets Mana to 0 if user tries and go lower than 0
+        elif new_mana >= 50:
+            new_mana = 50 # <-- Sets Mana to 50 if user tries and go higher than 50
         
+        self._mana = new_mana # <-- Sets new_mana to whatever number user enters
+            
+    @property
+    def level(self):
+        return self._level # Getter
+    
+    def level_up(self):
+        self._level += 1
+        self.health = 100
+        self.mana = 50
+        print(f"{self.name} leveled up to {self._level}!")
+    
+    def __str__(self):
+        return f"Name: {self._name}\nLevel: {self._level}\nHealth: {self._health}\nMana: {self._mana}\n"
+    
+hero = GameCharacter("Umisol")
+print(hero)
+
+hero.health -= 20
+hero.mana -= 5
+print(hero)
+
+hero.level_up()
+print(hero)
