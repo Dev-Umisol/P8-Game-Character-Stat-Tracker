bc832dd43d0ab4be54e55d040c9c53eb163e86d2
Dev-Umisol <148490822+Dev-Umisol@users.noreply.github.com>
2026-03-10 12:52:00 -0400

    basic frame of p8 done

--- P8GameCharacterStatTracker.py
+++ P8GameCharacterStatTracker.py
@@ -1,2 +1,32 @@
 class GameCharacter:
-    pass
+    
+    def __init__(self, name):
+        self._name = name
+        self._health = 100
+        self._mana = 50
+        self._level = 1
+    
+    @property
+    def name(self):
+        return self._name
+        
+    @property
+    def health(self):
+        return self._health # Getter
+    
+    @health.setter
+    def health(self, new_health):
+        if min(new_health) <= 0 or max(new_health) >= 100:
+            raise ValueError("Health can not be set to 0 or greater than 100")
+            
+        
+    @property
+    def mana(self):
+        return self._mana # Getter
+    
+    @mana.setter
+    def mana(self, new_mana):
+           if self.mana < 0:
+               raise ValueError("Mana can not be set to 0")
+           
+        
